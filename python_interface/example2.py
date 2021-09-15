fe494612f13b62778afe18bfd0cd4e4a83cc9a85
Kenneth Chen <kennethchen2001@gmail.com>
2021-09-14 23:03:28 -0700

    completed interface of python compiler

---
+++ python_interface/example2.py
@@ -0,0 +1,9 @@
+x = 3
+y = 4
+
+if x < y:
+    print(str(x) + " is less than " + str(y))
+elif x > y:
+    print(str(x) + " is greater than " + str(y))
+else:
+    print(str(x) + " is equal to " + str(y))
---
+++ python_interface/python_interface.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "interface.h"
+
+int main() {
+
+	Interface pySubInterpreter;
+
+	//Start the interface
+	pySubInterpreter.startInterface();
+
+	return 0;
+}
